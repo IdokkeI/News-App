b792233d8993dcfc57d0d3e8615868ddd63f96d0
Arseni-Lamihov <Arsenilamihau@gmail.com>
2020-10-06 23:08:01 +0300

    create form registr and login

--- news-client/src/App.js
+++ news-client/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import './App.css';
+import './Style/App.css';
+
+// import  render  from "react-dom";
+import  {BrowserRouter, Route} from "react-router-dom";
+
+import Registration from './registration';
+import Enter from './enter'
+import Home from './Home'
+
 
 function App() {
   return (
-    <div className="App">
 
+    <BrowserRouter>
+    <div>
+      <div className="Enter_registr">
+      <Route path="/" exact component={Home} />
+      <Route path="/registration" component={Registration} />
+      </div>
+      
+      <Route path="/enter" component={Enter} />
     </div>
-  );
+  </BrowserRouter>
+);
 }
 
 export default App;
