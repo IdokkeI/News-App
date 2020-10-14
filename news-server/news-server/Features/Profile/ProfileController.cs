d88d712add5df4396b0c6ef3268e556785ce0eef
Inquisitor1229 <36363019+Inquisitor1229@users.noreply.github.com>
2020-10-14 21:19:50 +0300

    get profile by id

---
+++ news-server/news-server/Features/Profile/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace news_server.Features.Profile
+{
+    //[Authorize]
+    //public class ProfileController: ApiController
+    //{
+    //    private readonly IProfileService profileService;
+
+    //    public ProfileController(IProfileService profileService)
+    //    {
+    //        this.profileService = profileService;
+    //    }
+
+    //    [HttpGet(nameof(GetUsers))]
+    //    public async Task<ActionResult> GetUsers()
+    //    {
+    //        var result = await profileService.GetUsers();
+    //        return Ok(result);
+    //    }
+    //}
+}
