7e8ca22c629198fe95477f28cbb01867a5c03aef
d1moza <gumbit9@mail.ru>
2020-10-16 00:43:14 +0300

    add demote interceptor (demoteFilter)

---
+++ news-server/news-server/Infrastructure/Filter/DemoteFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using news_server.Data.dbModels;
+using news_server.Infrastructure.Extensions;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace news_server.Infrastructure.Filter
+{
+    public class DemoteFilter : Attribute, IAsyncActionFilter
+    {
+        private readonly UserManager<User> userManager;
+
+        public DemoteFilter(UserManager<User> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            string username = context.HttpContext.User.GetUserName();
+            var user = await userManager.FindByNameAsync(username);
+            var role = (await userManager.GetRolesAsync(user)).FirstOrDefault();
+
+            var isInRole = context.HttpContext.User.IsInRole(role);
+            if (!isInRole)
+            {
+                context.Result = new StatusCodeResult(401);
+            }
+            else
+            {
+                await next();
+            }
+        }
+    }
+}
