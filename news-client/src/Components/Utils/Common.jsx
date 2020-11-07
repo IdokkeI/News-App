d6095f93647d648cdf1ed535fe87d7acdf86fb4f
d1moza <gumbit9@mail.ru>
2020-11-07 13:13:46 +0300

    small fix for saving data

--- news-client/src/Components/Utils/Common.jsx
+++ news-client/src/Components/Utils/Common.jsx
@@ -1,25 +1,25 @@
 // return the user data from the session storage
 export const getUser = () => {
-  const userStr = sessionStorage.getItem("user");
+  const userStr = localStorage.getItem("user");
   if (userStr) return JSON.parse(userStr);
   else return null;
 };
 
 // return the token from the session storage
 export const getToken = () => {
-  return sessionStorage.getItem("token") || null;
+  return localStorage.getItem("token") || null;
 };
 
 // remove the token and user from the session storage
 export const removeUserSession = () => {
-  sessionStorage.removeItem("token");
-  sessionStorage.removeItem("user");
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
 };
 
 // set the token and user from the session storage
 export const setUserSession = (token, data) => {
   if (JSON.stringify(data.username)) {
-    sessionStorage.setItem("token", token);
-    sessionStorage.setItem("user", JSON.stringify(data.username));
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(data.username));
   } else alert("Пользователя не существует");
 };
