3f703c899dc620b991e7490e5bbef9bcb2df20b2
d1moza <gumbit9@mail.ru>
2020-11-11 18:19:22 +0300

    deploy sh

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+echo "PULL FROM GIT"
+cd /home/News-App
+git pull
+cd news-server/news-server
+dotnet publish -o /var/www/appNews
+cd /home/News-App/news-client
+npm install
+pm2 restart news-server
+pm2 restart news-client
+echo "END"
