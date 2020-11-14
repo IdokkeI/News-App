683f6aa0eb5b45c019af5a37f277e713bc7c2116
IdokkeI <62292706+IdokkeI@users.noreply.github.com>
2020-11-14 16:29:32 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,10 @@
 
 ssh root@$IP <<EOF
 echo "PULL FROM GIT!!!"
-cd /home/News-App
-git add .
-git commit -m "fs8r"
-git pull
-cd news-server/news-server
+cd /home
+rm -rf News-App
+git clone https://github.com/IdokkeI/News-App.git
+cd News-App/news-server/news-server
 dotnet publish -o /var/www/appNews
 cd /home/News-App/news-client
 npm install
