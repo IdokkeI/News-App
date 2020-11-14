b90f39773ce867241ee8d80475b271c5cfb9c6d1
IdokkeI <62292706+IdokkeI@users.noreply.github.com>
2020-11-15 01:57:37 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ echo "Welcome to the club buddy"
 ls -l
 cd news-server
 ls -l
-dotnet build news-server.sln
+dotnet publish news-server.sln
 cd news-server/bin/Debug/netcoreapp3.1
 ls -l
 scp -rp news-server/news-server/bin/Debug/netcoreapp3.1 root@$server:/home/BackNews/
