2a3000e9b9ebd7795f7d2dd4a96a5625f5fec537
IdokkeI <62292706+IdokkeI@users.noreply.github.com>
2020-11-15 09:40:54 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,11 @@ dotnet publish news-server.sln
 cd news-server/bin/Debug/netcoreapp3.1/publish
 echo "#############!!!!!!"
 scp -rp ./* root@$server:/var/www/appNews/
+scp -rp /news-client/* root@$server:/home/FrontNews/
+ssh root@$IP <<EOF
+ npm i
+ pm2 restart news-server
+ pm2 restart news-client
+EOF
+
 echo "END!!!"
