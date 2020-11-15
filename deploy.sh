239e7c5673cd0a51b1689590a331572ecc541bde
IdokkeI <62292706+IdokkeI@users.noreply.github.com>
2020-11-15 09:24:10 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,5 @@ cd news-server
 dotnet publish news-server.sln
 cd news-server/bin/Debug/netcoreapp3.1/publish
 echo "#############!!!!!!"
-ls -l
 scp -rp ./* root@$server:/var/www/appNews/
 echo "END!!!"
