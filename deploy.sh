9741e900bea8ad3a626a368c871b11a072692dc0
IdokkeI <62292706+IdokkeI@users.noreply.github.com>
2020-11-15 09:51:19 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Welcome to the club buddy"
+cd news-client
+npm i
+scp -rp ./* root@$server:/home/FrontNews/
+cd..
 cd news-server
 dotnet publish news-server.sln
 cd news-server/bin/Debug/netcoreapp3.1/publish
@@ -7,10 +11,8 @@ echo "#############!!!!!!"
 scp -rp ./* root@$server:/var/www/appNews/
 cd /news-client
 ls -l
-ssh root@$IP <<EOF
- npm i
+ssh root@$server <<EOF
  pm2 restart news-server
  pm2 restart news-client
 EOF
-
 echo "END!!!"
