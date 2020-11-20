25d9c1ece73506d9cf487c2363a93e076f99d209
Inquisitor1229 <36363019+Inquisitor1229@users.noreply.github.com>
2020-11-20 09:28:20 +0300

    Revert "add quill package and update newsworkspace"
    This reverts commit ee944ce521c7896077ed00b75769511fef42a045.

--- news-client/src/Components/NewsWorkspace/NewsWorkspace.jsx
+++ news-client/src/Components/NewsWorkspace/NewsWorkspace.jsx
@@ -1,42 +1,9 @@
 import React from 'react'
 import './NewsWorkspace.scss'
-<<<<<<< Updated upstream
-=======
-import {
-    Route,
-    useParams
-} from "react-router-dom"
-import News from "./News/News"
-import New from "./News/New/New"
-import CreatePost from "../Header/CreatePost/CreatePost"
-import UserNotifications from "../Header/Profile/UserNotifications/UserNotifications"
-import UserNews from "../Header/Profile/UserNews/UserNews"
-import UserSubscribers from "../Header/Profile/UserSubscribers/UserSubscribers"
 
-
-function NewItem(props) {
-    let { slug } = useParams();
-    return <New id={slug} state={props.state} dispatch={props.dispatch} store={props.store}/>;
-}
->>>>>>> Stashed changes
-
-const NewsWorkspace = (props) => {
+const NewsWorkspace = () => {
     return <div className='newsWorkspace'>
-<<<<<<< Updated upstream
 
-=======
-        <div className='newsWorkspace-container'>
-            <Route path='/CreatePost' render ={ ()=> <CreatePost state={props.state} dispatch={props.dispatch} store={props.store}/> } />
-            <Route path='/' exact render ={ ()=> <News /> } />
-            <Route path='/New' exact render ={ ()=> <New id = {Route.context.params('id')} state={props.state} dispatch={props.dispatch} store={props.store}/> } />
-            <Route path='/UserNotifications' exact render ={ () => <UserNotifications /> } />
-            <Route path="/NewItem/:slug">
-                <NewItem state={props.state} dispatch={props.dispatch} store={props.store}/>
-            </Route>
-            <Route path="/userSubscribers" component={UserSubscribers} />
-            <Route exact path="/userNews" component={UserNews} />
-        </div>
->>>>>>> Stashed changes
     </div>
 }
 
